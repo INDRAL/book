463453880f6b9e9d6490d4d6ca8d56fe1387e808
INDRAL <indral10794@gmail.com>
2024-07-11 14:52:47 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -2,28 +2,15 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-import { Grid } from '@mui/material';
-import Divider from '@material-ui/core/Divider';
-import CardContent from '@mui/material/CardContent';
-import {CardActions} from '@mui/material';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
 import { useNavigate } from 'react-router-dom';
 
 import Card from '@mui/material/Card';
 import CardMedia from '@mui/material/CardMedia';
 const pages = ['Login'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
-function Home() {
     const navigate = useNavigate();
   const openmenu=()=>{
     navigate('/Login');
