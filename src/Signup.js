99c0f49876d5b4024f8f9e4de6aba16cb1f5d7c3
INDRAL <indral10794@gmail.com>
2024-07-11 14:54:06 +0530

    Update Signup.js

--- src/Signup.js
+++ src/Signup.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
 import { useState } from "react"
 import { useNavigate } from 'react-router-dom';
 import './login.css';
