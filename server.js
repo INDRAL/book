43963eb08e5163d7bad3acd126b5e57c1bfccb6a
INDRAL <indral10794@gmail.com>
2024-07-12 15:49:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,13 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
+});
 app.post("/register", async function(req, res)
 {
   const {email,paswd,name } = req.body;
